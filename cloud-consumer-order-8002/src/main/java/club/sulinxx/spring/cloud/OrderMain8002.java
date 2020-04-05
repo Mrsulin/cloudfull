1e8775464b776c6f5a8f59a362ac6ef6ed2f6e76
Sulin <1848424208@qq.com>
2020-04-06 01:46:01 +0800

    消费者2个注册（注册地址后面必须是eureka）

--- cloud-consumer-order-8002/src/main/java/club/sulinxx/spring/cloud/OrderMain8002.java
+++ cloud-consumer-order-8002/src/main/java/club/sulinxx/spring/cloud/OrderMain8002.java
@@ -2,12 +2,13 @@ package club.sulinxx.spring.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * Title: OrderMain8002
  */
-@SpringBootApplication()
+@SpringBootApplication
+@EnableEurekaClient
 public class OrderMain8002 {
     public static void main(String[] args) {
         SpringApplication.run(OrderMain8002.class);
--- cloud-provider-payment-8001/src/main/java/club/sulinxx/spring/cloud/PaymentMain8001.java
+++ cloud-provider-payment-8001/src/main/java/club/sulinxx/spring/cloud/PaymentMain8001.java
@@ -2,11 +2,14 @@ package club.sulinxx.spring.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * Title: PaymentMain8001
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class PaymentMain8001 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentMain8001.class);
